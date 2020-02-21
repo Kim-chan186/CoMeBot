08517d33497f47867f80af412b9533cc6fbb052f
annie1229 <53402709+annie1229@users.noreply.github.com>
2020-02-21 21:16:21 +0900

    Update gui_test.h

--- gui_test.h
+++ gui_test.h
@@ -7,40 +7,38 @@
 
 #include <opencv2/opencv.hpp>
 #include <iostream>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
 #include <cmath>
 #define PI 3.14
 
 using namespace cv;
 using namespace std;
 
-Scalar black(0, 0, 0);
-Scalar red(0, 0, 255);
-Scalar green(0, 180, 0);
-Scalar blue(255, 0, 0);
-Scalar white(255, 255, 255);
-Scalar yellow(0, 255, 255);
-Scalar cyan(255, 255, 0);
-Scalar magenta(255, 0, 255);
-Scalar gray(150, 150, 150);
-Scalar purple(255, 0, 127);
-
-typedef struct {
-	double r;       // a fraction between 0 and 1
-	double g;       // a fraction between 0 and 1
-	double b;       // a fraction between 0 and 1
-} rgb;
-
-typedef struct {
-	double h;       // angle in degrees
-	double s;       // a fraction between 0 and 1
-	double v;       // a fraction between 0 and 1
-} hsv;
-
-static rgb   hsv2rgb(hsv in);
-
 namespace Gui {
+	Scalar black(0, 0, 0);
+	Scalar red(0, 0, 255);
+	Scalar green(0, 180, 0);
+	Scalar blue(255, 0, 0);
+	Scalar white(255, 255, 255);
+	Scalar yellow(0, 255, 255);
+	Scalar cyan(255, 255, 0);
+	Scalar magenta(255, 0, 255);
+	Scalar gray(150, 150, 150);
+	Scalar purple(255, 0, 127);
+
+	typedef struct {
+		double r;       // a fraction between 0 and 1
+		double g;       // a fraction between 0 and 1
+		double b;       // a fraction between 0 and 1
+	} rgb;
+
+	typedef struct {
+		double h;       // angle in degrees
+		double s;       // a fraction between 0 and 1
+		double v;       // a fraction between 0 and 1
+	} hsv;
+
+	static rgb   hsv2rgb(hsv in);
+
 	Mat readimg(String name, int x, int y);
 	Point percent(Point emotion);
 	Scalar deg2hue(int x, int y);
@@ -121,7 +119,7 @@ Scalar Gui::deg2hue(int x, int y) //(감정 x좌표, 감정 y좌표)
 }
 
 //hsv->rgb로 바꾸는 함수
-rgb Gui::hsv2rgb(hsv in)
+Gui::rgb Gui::hsv2rgb(hsv in)
 {
 	double      hh, p, q, t, ff;
 	long        i;
