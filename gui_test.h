902bdb9b70c13f5edc6ecb8b0ea41d58fe3ef777
annie1229 <53402709+annie1229@users.noreply.github.com>
2020-02-21 22:17:21 +0900

    gui수정

--- gui_test.h
+++ gui_test.h
@@ -190,51 +190,6 @@ Gui::rgb Gui::hsv2rgb(hsv in)
 	return out;
 }
 
-//rgb->hsv로 바꾸는 함수
-Gui::hsv Gui::rgb2hsv(rgb in)
-{
-	hsv        out;
-	double      min, max, delta;
-
-	min = in.r < in.g ? in.r : in.g;
-	min = min  < in.b ? min : in.b;
-
-	max = in.r > in.g ? in.r : in.g;
-	max = max  > in.b ? max : in.b;
-
-	out.v = max;                                // v
-	delta = max - min;
-	if (max > 0.0) { // NOTE: if Max is == 0, this divide would cause a crash
-		out.s = (delta / max);                  // s
-	}
-	else {
-		// if max is 0, then r = g = b = 0
-		// s = 0, v is undefined
-		out.s = 0.0;
-		out.h = 0.0;                            // its now undefined
-		return out;
-	}
-	if (in.r >= max)                           // > is bogus, just keeps compilor happy
-		if (delta == 0) {
-			out.h = 0.0;
-		}
-		else {
-			out.h = (in.g - in.b) / delta;        // between yellow & magenta
-		}
-	else
-		if (in.g >= max)
-			out.h = 2.0 + (in.b - in.r) / delta;  // between cyan & yellow
-		else
-			out.h = 4.0 + (in.r - in.g) / delta;  // between magenta & cyan
-
-	out.h *= 60.0;                              // degrees
-
-	if (out.h < 0.0)
-		out.h += 360.0;
-
-	return out;
-}
-
 //좌표값에 따라 화살표그려주는 함수
 void Gui::color_line_chart(Mat img, Point emotion)  //(whale원형그래프이미지, 감정값)
 {
