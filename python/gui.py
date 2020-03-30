f775b2800ab6e01b79b9b173794034e98116a0a9
Kim-chan186 <kch1051104@naver.com>
2020-03-30 21:37:45 +0900

    오탈자 수정
    오탈자 수정

--- python/gui.py
+++ python/gui.py
@@ -18,17 +18,16 @@ from PIL import ImageFont, ImageDraw, Image
 import numpy as np
 import cv2 as cv
 
+whale_path = "img/whale.jpg"  # 원형그래프 이미지 경로
+stick_path = "img/stick.jpg"  # 막대그래프 이미지 경로
+emotion_grid_path = "img/emotion_grid.jpg"  # emotion_grid 이미지 경로
+white_bg_path = "img/white_bg.jpg"  # 흰배경 이미지 경로
+black_bg_path = "img/black_bg.jpg"  # 검은배경 이미지 경로
 
-whale_path = "img/whale.jpg"    #원형그래프 이미지 경로
-stick_path = "img/stick.jpg"    #막대그래프 이미지 경로
-emotion_grid_path = "img/emotion_grid.jpg"  #emotion_grid 이미지 경로
-white_bg_path = "img/white_bg.jpg"  #흰배경 이미지 경로
-black_bg_path = "img/black_bg.jpg"  #검은배경 이미지 경로
-
-font_path = "./malgun.ttf"  #폰트경로
+font_path = "./malgun.ttf"  # 폰트경로
 
 PI = 3.14
-#scalar 색 정의(b,g,r)
+# scalar 색 정의(b,g,r)
 black = (0, 0, 0)
 red = (0, 0, 255)
 green = (0, 180, 0)
@@ -72,22 +71,21 @@ class brain_data(NamedTuple):
     font_scale: int
 
 
-color_img = cv.imread(whale_path, cv.IMREAD_COLOR) #원형색상그래프 이미지 불러오기
+color_img = cv.imread(whale_path, cv.IMREAD_COLOR)  # 원형색상그래프 이미지 불러오기
 if color_img is None:
     print("Can't open whale.jpg")
-stick_img = cv.imread(stick_path, cv.IMREAD_COLOR) #막대그래프 이미지 불러오기
+stick_img = cv.imread(stick_path, cv.IMREAD_COLOR)  # 막대그래프 이미지 불러오기
 if stick_img is None:
     print("Can't open stick.jpg")
-emotion_grid = cv.imread(emotion_grid_path, cv.IMREAD_COLOR)   #emotion_grid 이미지 불러오기
+emotion_grid = cv.imread(emotion_grid_path, cv.IMREAD_COLOR)  # emotion_grid 이미지 불러오기
 if emotion_grid is None:
     print("Can't open emotion_grid.jpg")
-black_bg = cv.imread(black_bg_path, cv.IMREAD_COLOR)   #검정배경 불러오기
+black_bg = cv.imread(black_bg_path, cv.IMREAD_COLOR)  # 검정배경 불러오기
 if black_bg is None:
     print("Can't open black_bg.jpg")
-white_bg = cv.imread(white_bg_path, cv.IMREAD_COLOR)   #하얀배경 불러오기
+white_bg = cv.imread(white_bg_path, cv.IMREAD_COLOR)  # 하얀배경 불러오기
 if white_bg is None:
     print("Can't open white_bg.jpg")
-
 
 pre_text = []
 pre_data = []
@@ -123,13 +121,14 @@ def print_typo():
             brain_img = pre_data[i].img
         else:
             data = pre_data[i]
-            data = brain_data(brain_img, data.word, data.hue, data.transparency, data.point, data.font_scale)#img파일 수정
+            data = brain_data(brain_img, data.word, data.hue, data.transparency, data.point,
+                              data.font_scale)  # img파일 수정
             pre_data[i] = whale_brain(data)
             brain_img = pre_data[i].img
 
     cv.imshow("whale brain", brain_img)
-    del pre_data[:] #리스트 초기화
-    del pre_text[:] #리스트 초기화
+    del pre_data[:]  # 리스트 초기화
+    del pre_text[:]  # 리스트 초기화
 
 
 def del_typo(text):
@@ -140,47 +139,47 @@ def del_typo(text):
             data = brain_data(white_bg, '', data.hue, 0, data.point, 0)
             pre_data[i] = whale_brain(data)
             print_typo()
-            #print("삭제완료")
+            # print("삭제완료")
             flag = 1
     if flag == 0:
         print("error: 입력하신 단어는 존재하지 않습니다")
 
 
-#감정값 whale이미지에 맞게 비율 맞추는 함수
+# 감정값 whale이미지에 맞게 비율 맞추는 함수
 def percent(emotion):
     x = emotion[0]
     y = emotion[1]
-    x = float(x) * (379.0/100.0)
-    y = float(y) * (379.0/100.0)
-    return (x,y)
+    x = float(x) * (379.0 / 100.0)
+    y = float(y) * (379.0 / 100.0)
+    return (x, y)
 
 
-#좌표값->각도로 변환하는 함수
+# 좌표값->각도로 변환하는 함수
 def xy2deg(x, y):
     degree = float(atan2(x, y) * 180 / PI)
     return degree
 
 
-#좌표각도->hue값으로 변환해주는 함수
+# 좌표각도->hue값으로 변환해주는 함수
 def deg2hue(degree):
-    #각도값이 -180~180 범위로 표현되기 때문에 0~360도로 변환하기위한 조건문
+    # 각도값이 -180~180 범위로 표현되기 때문에 0~360도로 변환하기위한 조건문
     if degree >= 0:
-        hue = int(degree)/2
+        hue = int(degree) / 2
     else:
         degree += 360.0
-        hue = int(degree)/2
+        hue = int(degree) / 2
 
-    #hue값 color(bgr)로 변환
-    Phsv = hsv(hue*2, 1, 1)
+    # hue값 color(bgr)로 변환
+    Phsv = hsv(hue * 2, 1, 1)
     Prgb = hsv2rgb(Phsv)
     r = int(Prgb.r * 255)
     g = int(Prgb.g * 255)
     b = int(Prgb.b * 255)
 
-    return (b,g,r)
+    return (b, g, r)
 
 
-#hsv->rgb로 바꾸는 함수
+# hsv->rgb로 바꾸는 함수
 def hsv2rgb(hsv):
     input = hsv
 
@@ -214,7 +213,7 @@ def hsv2rgb(hsv):
     return output
 
 
-#rgb->hsv로 바꾸는 함수
+# rgb->hsv로 바꾸는 함수
 def rgb2hsv(rgb):
     input = rgb
 
@@ -223,7 +222,7 @@ def rgb2hsv(rgb):
     else:
         min = input.g
 
-    if min <input.b:
+    if min < input.b:
         min = min
     else:
         min = input.b
@@ -267,70 +266,70 @@ def rgb2hsv(rgb):
     return hsv(h, s, v)
 
 
-#좌표값에 따라 화살표그려주는 함수
+# 좌표값에 따라 화살표그려주는 함수
 def color_line_chart(img, emotion):
     img_circle = img.copy()
     emotion = percent(emotion)
     emotion_x = emotion[0]
     emotion_y = emotion[1]
-    h , w = img_circle.shape[:2]
-    #이미지 중심좌표저장
+    h, w = img_circle.shape[:2]
+    # 이미지 중심좌표저장
     center_x = int(w / 2)
     center_y = int(h / 2)
 
-    result_x = int(center_x + emotion_x) #감정값의 원점을 이미지 중심으로 옮김
-    result_y = int(w - (center_y + emotion_y))   #y좌표의 0점이 화면상단에서 시작하므로 반전시킴
+    result_x = int(center_x + emotion_x)  # 감정값의 원점을 이미지 중심으로 옮김
+    result_y = int(w - (center_y + emotion_y))  # y좌표의 0점이 화면상단에서 시작하므로 반전시킴
 
-    color = deg2hue(xy2deg(emotion_x, emotion_y))    #좌표에 따른 bgr값 받아옴
+    color = deg2hue(xy2deg(emotion_x, emotion_y))  # 좌표에 따른 bgr값 받아옴
 
-    cv.circle(img_circle, (center_x, center_y), 6, gray, -1) #그래프 중심
-    cv.arrowedLine(img_circle, (center_x, center_y), (result_x, result_y), color, 5, cv.CV_8UC3, 0, 0.1)    #화살표그리기
+    cv.circle(img_circle, (center_x, center_y), 6, gray, -1)  # 그래프 중심
+    cv.arrowedLine(img_circle, (center_x, center_y), (result_x, result_y), color, 5, cv.CV_8UC3, 0, 0.1)  # 화살표그리기
 
     return img_circle
 
 
-#좌표값을 막대그래프로 나타내주는 함수
+# 좌표값을 막대그래프로 나타내주는 함수
 def stick_chart(img, mood, energy):
     img_stick = img.copy()
-    #감정값(-100~100범위를 막대그래프 -180~180범위로 나타내는식
-    result_x = int(mood * (180.0/100.0))
-    result_y = int(energy * (180.0/100.0))
+    # 감정값(-100~100범위를 막대그래프 -180~180범위로 나타내는식
+    result_x = int(mood * (180.0 / 100.0))
+    result_y = int(energy * (180.0 / 100.0))
 
-    if mood >= 0:   #감정의 x좌표(mood)가 양수이면 초록색으로 막대그래프 채움
+    if mood >= 0:  # 감정의 x좌표(mood)가 양수이면 초록색으로 막대그래프 채움
         img_stick = draw_rect(img_stick, 250, result_x, green)
-    else:   #감정의 x좌표(mood)가 음수이면 보라색으로 막대그래프 채움
+    else:  # 감정의 x좌표(mood)가 음수이면 보라색으로 막대그래프 채움
         img_stick = draw_rect(img_stick, 250, result_x, purple)
-    if energy >= 0: #감정의 y좌표(energy)가 양수이면 빨간색으로 막대그래프 채움
+    if energy >= 0:  # 감정의 y좌표(energy)가 양수이면 빨간색으로 막대그래프 채움
         img_stick = draw_rect(img_stick, 420, result_y, red)
-    else:   #감정의 y좌표(energy)가 음수이면 파란색으로 막대그래프 채움
+    else:  # 감정의 y좌표(energy)가 음수이면 파란색으로 막대그래프 채움
         img_stick = draw_rect(img_stick, 420, result_y, blue)
 
     return img_stick
 
 
-#stick_chart에 사용되는 직사각형 그리기 함수
+# stick_chart에 사용되는 직사각형 그리기 함수
 def draw_rect(img, p, emotion, color):
     img_clone = img.copy()
-    center_y = 200  #막대그래프 중심축
+    center_y = 200  # 막대그래프 중심축
 
     if emotion >= 0:
         color = trans_color((emotion / 2), color)
     else:
         color = trans_color((emotion / 2), color)
 
-    cv.rectangle(img_clone, (p+2,center_y), (p+88,center_y - emotion), color, -1)
+    cv.rectangle(img_clone, (p + 2, center_y), (p + 88, center_y - emotion), color, -1)
 
     return img_clone
 
 
-#막대바 색 채도 변경 함수
+# 막대바 색 채도 변경 함수
 def trans_color(emotion, color):
     if emotion < 0:
         emotion *= (-1)
 
     percent = double(emotion) / 100.0
     percent = (percent / 2 + 0.5)
-    #감정정도(percent)에 따라 color 채도 변화
+    # 감정정도(percent)에 따라 color 채도 변화
     Prgb = rgb(double(color[2]) / 255.0, double(color[1]) / 255.0, double(color[0]) / 255.0)
     Phsv = rgb2hsv(Prgb)
     Phsv = hsv(Phsv.h, Phsv.s * percent, Phsv.v)
@@ -342,16 +341,16 @@ def trans_color(emotion, color):
     return (b, g, r)
 
 
-#text 색 채도 변경 함수
+# text 색 채도 변경 함수
 def trans_text_color(emotion, color, b_n_w):
     percent = double(emotion) / 100.0
-    #감정정도(percent)에 따라 color 채도 변화
+    # 감정정도(percent)에 따라 color 채도 변화
     Prgb = rgb(double(color[2]) / 255.0, double(color[1]) / 255.0, double(color[0]) / 255.0)
     Phsv = rgb2hsv(Prgb)
 
-    if b_n_w == 1:  #흰배경일때
+    if b_n_w == 1:  # 흰배경일때
         Phsv = hsv(Phsv.h, Phsv.s * percent, Phsv.v)
-    else:   #검은배경일때
+    else:  # 검은배경일때
         Phsv = hsv(Phsv.h, Phsv.s, Phsv.v * percent)
 
     Prgb = hsv2rgb(Phsv)
@@ -362,23 +361,23 @@ def trans_text_color(emotion, color, b_n_w):
     return (b, g, r)
 
 
-#입력받은 감정단어를 emotion_grid에 점찍어주는 함수
+# 입력받은 감정단어를 emotion_grid에 점찍어주는 함수
 def emotion_word_point(img, p):
     img_grid = img.copy()
 
-    color = emotion_word_color(p)   #word의 위치좌표에 맞는 색상을 반환하는 함수
-    cv.circle(img_grid,(p.x,p.y),4,color,-1)
+    color = emotion_word_color(p)  # word의 위치좌표에 맞는 색상을 반환하는 함수
+    cv.circle(img_grid, (p.x, p.y), 4, color, -1)
 
     return img_grid
 
 
-#단어의 좌표 위치에 따른 색상 변환함수
+# 단어의 좌표 위치에 따른 색상 변환함수
 def emotion_word_color(p):
     zeroP = Point(424, 368)
-    #word_x,word_y은 deg2hue함수를 사용하기 위한 좌표값을 위한 변수
+    # word_x,word_y은 deg2hue함수를 사용하기 위한 좌표값을 위한 변수
     word_x = p.x
     word_y = p.y
-    #word값을 emotion값으로 변화시켜주는 계산식
+    # word값을 emotion값으로 변화시켜주는 계산식
     word_x -= zeroP.x
     word_y -= zeroP.y
     word_y *= (-1)
@@ -389,12 +388,12 @@ def emotion_word_color(p):
 def whale_brain(data):
     brain = data.img.copy()
 
-    hue = float(450 - (data.hue *2))
+    hue = float(450 - (data.hue * 2))
     if hue > 360:
         hue -= 360
 
-    color = deg2hue(hue)    #hue값 두배해서 넣어주는거임(==degree)
-    color = trans_text_color(data.transparency, color, 1) #투명도(0~100),색,(검은배경=0,흰배경=1)
+    color = deg2hue(hue)  # hue값 두배해서 넣어주는거임(==degree)
+    color = trans_text_color(data.transparency, color, 1)  # 투명도(0~100),색,(검은배경=0,흰배경=1)
 
     p = data.point
     if p[0] == 0:
@@ -415,10 +414,10 @@ def whale_brain(data):
     img_pil = Image.fromarray(brain)
     draw = ImageDraw.Draw(img_pil)
     size = font.getsize(text)
-    #print(size)
+    # print(size)
     draw.text((x - (size[0] / 2), y - (size[1] / 2)), text, font=font, fill=color)
     brain = np.array(img_pil)
-    #cv.circle(brain,(x,y),5,black,-1)
+    # cv.circle(brain,(x,y),5,black,-1)
 
     '''
     #puttext사용
@@ -434,7 +433,7 @@ def whale_brain(data):
     '''
 
     data = brain_data(brain, text, data.hue, data.transparency, data.point, font_scale)
-    #cv.imshow("whale brain",brain)
+    # cv.imshow("whale brain",brain)
 
     return data
 
@@ -446,21 +445,22 @@ def hangulfont(data):
     font = ImageFont.truetype(fontpath, 20)
     img_pil = Image.fromarray(text_img)
     draw = ImageDraw.Draw(img_pil)
-    draw.text((60, 60), "안녕하세요", font=font, fill = (255, 255, 255, 50))
+    draw.text((60, 60), "안녕하세요", font=font, fill=(255, 255, 255, 50))
 
     cv.imshow("hangul", text_img)
 
-#세가지의 창을 하나로 합치는 함수
-def combine_imshow(circleL, circleS, stick)->object :
+
+# 세가지의 창을 하나로 합치는 함수
+def combine_imshow(circleL, circleS, stick) -> object:
     circleL = cv.resize(circleL, (600, 600))
     circleS = cv.resize(circleS, (380, 380))
     stick = cv.resize(stick, (380, 220))
-    #cv.imshow("circleL",circleL)
-    #cv.imshow("circleS",circleS)
-    #cv.imshow("stick",stick)
-    #opencv함수인 vconcat,hconcat함수가 제대로 작동하지 않아 numpy의 vstack,hstack함수로 변경
-    #vertical = cv.vconcat(circleS,stick)
-    #horizontal = cv.hconcat(circleL,vertical)
+    # cv.imshow("circleL",circleL)
+    # cv.imshow("circleS",circleS)
+    # cv.imshow("stick",stick)
+    # opencv함수인 vconcat,hconcat함수가 제대로 작동하지 않아 numpy의 vstack,hstack함수로 변경
+    # vertical = cv.vconcat(circleS,stick)
+    # horizontal = cv.hconcat(circleL,vertical)
     vertical = np.vstack((circleS, stick))
     horizontal = np.hstack((circleL, vertical))
     cv.imshow("result", horizontal)
@@ -482,7 +482,7 @@ if __name__ == "__main__":
     brain_typo('봄이다', 0, 80, (70, -30), 20)
     print_typo()
     cv.waitKey(0)
-    #brain_typo('잘되는구만', 70, 20, (0, 0), 10)
+    # brain_typo('잘되는구만', 70, 20, (0, 0), 10)
     brain_typo('잘되는구만', 70, 100, (0, 50), 50)
     print_typo()
     cv.waitKey(0)
