e12c4c95346d645d9982c3708398ffa2961c730f
annie1229 <53402709+annie1229@users.noreply.github.com>
2020-03-31 16:23:16 +0900

    brain_typo리스트로 입력받도록 수정, gui_main함수명 변경

--- python/gui.py
+++ python/gui.py
@@ -1,15 +1,16 @@
 """
 ※main함수에서 사용법
 pip install pillow로 모듈설치
-from gui import gui_main,brain_typo 함수를 추가한다
-이미지 파일 경로(대략 22~26줄)은 자신의 폴더에 맞게 수정바람,폰트(28줄)도 프로젝트 경로에 추가해주세요!
-gui_main((mode좌표),(emotion좌표),mood값,energy값)    ex)gui_main((50,50),(-30,-70),-60,30)
-brain_typo('문장',hue값,투명도,(문장위치좌표),폰트크기) ex)brain_typo('반가워',70,100,(70,-30),40)
+from gui import gui_graph,brain_typo,print_typo 함수를 추가한다
+이미지 파일 경로(대략 23~27줄)은 자신의 폴더에 맞게 수정바람,폰트(30줄)도 프로젝트 경로에 추가해주세요!
+gui_graph((mode좌표),(emotion좌표),mood값,energy값)    ex)gui_graph((50,50),(-30,-70),-60,30)
+brain_typo(list)  ex)data = ['반가워', 70, 100, (70, -30), 40]이런식으로 리스트에 데이터를 담고, brain_typo(data)
+-->list구성: list = ['문장',hue값,투명도,(문장위치좌표),폰트크기]
 -->투명도는 0(투명)~100(불투명)사이의값
 -->단어위치좌표 원점(0,0) max는(100,100), min는(-100,-100)임
 -->글자 폰트 크기는 30~50정도가 적당한 사이즈 딱히 범위 제한은 없음 필요한 사이즈로 조절하시길
 (del_typo함수는 print_typo 함수 실행 이후에 모든 리스트가 초기화되므로 필요가 없어졌음)
-참고)458줄에 __main__ 코드 작성(cv.Waitkey(0)있어서 화면넘기면서 변화확인)
+참고)458줄에 __main__ 코드 작성(cv.waitkey(0)있어서 화면넘기면서 변화확인)
 """
 from math import atan2
 from numpy import double
@@ -18,11 +19,13 @@ from PIL import ImageFont, ImageDraw, Image
 import numpy as np
 import cv2 as cv
 
-whale_path = "img/whale.jpg"  # 원형그래프 이미지 경로
-stick_path = "img/stick.jpg"  # 막대그래프 이미지 경로
+
+whale_path = "img/whale.jpg"    # 원형그래프 이미지 경로
+stick_path = "img/stick.jpg"    # 막대그래프 이미지 경로
 emotion_grid_path = "img/emotion_grid.jpg"  # emotion_grid 이미지 경로
 white_bg_path = "img/white_bg.jpg"  # 흰배경 이미지 경로
 black_bg_path = "img/black_bg.jpg"  # 검은배경 이미지 경로
+# transparency_bg_path = "img/transparency.png"  # 투명배경 이미지 경로
 
 font_path = "./malgun.ttf"  # 폰트경로
 
@@ -57,92 +60,91 @@ class rgb(NamedTuple):
     b: double = 0.0
 
 
-class EWords(NamedTuple):
-    p: Point
-    text: str
-
-
-class brain_data(NamedTuple):
-    img: np.array
-    word: str
-    hue: float
-    transparency: int
-    point: Point
-    font_scale: int
-
-
-color_img = cv.imread(whale_path, cv.IMREAD_COLOR)  # 원형색상그래프 이미지 불러오기
+color_img = cv.imread(whale_path, cv.IMREAD_COLOR)   # 원형색상그래프 이미지 불러오기
 if color_img is None:
     print("Can't open whale.jpg")
-stick_img = cv.imread(stick_path, cv.IMREAD_COLOR)  # 막대그래프 이미지 불러오기
+stick_img = cv.imread(stick_path, cv.IMREAD_COLOR)   # 막대그래프 이미지 불러오기
 if stick_img is None:
     print("Can't open stick.jpg")
-emotion_grid = cv.imread(emotion_grid_path, cv.IMREAD_COLOR)  # emotion_grid 이미지 불러오기
+emotion_grid = cv.imread(emotion_grid_path, cv.IMREAD_COLOR)   # emotion_grid 이미지 불러오기
 if emotion_grid is None:
     print("Can't open emotion_grid.jpg")
-black_bg = cv.imread(black_bg_path, cv.IMREAD_COLOR)  # 검정배경 불러오기
+black_bg = cv.imread(black_bg_path, cv.IMREAD_COLOR)   # 검정배경 불러오기
 if black_bg is None:
     print("Can't open black_bg.jpg")
-white_bg = cv.imread(white_bg_path, cv.IMREAD_COLOR)  # 하얀배경 불러오기
+white_bg = cv.imread(white_bg_path, cv.IMREAD_COLOR)   # 하얀배경 불러오기
 if white_bg is None:
     print("Can't open white_bg.jpg")
+'''
+transparency_bg = cv.imread(transparency_bg_path, cv.IMREAD_UNCHANGED)
+if transparency_bg is None:
+    print("Can't open transparency.png")
+'''
 
-pre_text = []
 pre_data = []
 
 
-def gui_main(mode, emotion, mood, energy):
+def gui_graph(mode, emotion, mood, energy):
     circleL = color_line_chart(color_img, mode)
     circleS = color_line_chart(color_img, emotion)
     stick = stick_chart(stick_img, mood, energy)
     combine_imshow(circleL, circleS, stick)
 
 
-def brain_typo(text, hue, transparency, point, font_scale):
-    img = white_bg  # 기본배경 설정
-    # print(type(img))
-    data = brain_data(img, text, hue, transparency, point, font_scale)  # 입력받은 값 data변수에 담기
-
-    if text not in pre_text:
-        after_data = whale_brain(data)
-        pre_data.append(after_data)
-        pre_text.append(text)
+def brain_typo(typo_data):
+    # typo_data = [text, hue, transparency, point, font_scale]
+    if len(typo_data) != 5:
+        print("brain_typo error: Input variable not entered correctly")
     else:
-        for i in range(len(pre_data)):
-            if pre_text[i] == text:
-                after_data = whale_brain(data)
-                pre_data[i] = after_data
-                # print("중복있음")
+        pre_data.append(typo_data)
 
 
+# text, hue, transparency, point, font_scale
 def print_typo():
+    brain = white_bg.copy()
+    #trans = transparency_bg.copy()
     for i in range(len(pre_data)):
-        if i == 0:
-            brain_img = pre_data[i].img
+        data = pre_data[i]
+
+        hue = float(450 - (data[1] * 2))
+        if hue > 360:
+            hue -= 360
+        color = deg2hue(hue)  # hue값 두배해서 넣어주는거임(==degree)
+        color = trans_text_color(data[2], color, 1)  # 투명도(0~100),색,(검은배경=0,흰배경=1)
+
+        # 텍스트 위치좌표 중심으로 옮기는 계산식
+        p = data[3]
+        if p[0] == 0:
+            x = 0
         else:
-            data = pre_data[i]
-            data = brain_data(brain_img, data.word, data.hue, data.transparency, data.point,
-                              data.font_scale)  # img파일 수정
-            pre_data[i] = whale_brain(data)
-            brain_img = pre_data[i].img
+            x = p[0] * 3
+        if p[1] == 0:
+            y = 0
+        else:
+            y = p[1] * 3
+        x += 300
+        y = 600 - (y + 300)
 
-    cv.imshow("whale brain", brain_img)
+        # 텍스트 출력하는 부분
+        text = data[0]
+        font_scale = data[4]
+        fontpath = font_path
+        font = ImageFont.truetype(fontpath, font_scale)
+        img_pil = Image.fromarray(brain)
+        draw = ImageDraw.Draw(img_pil)
+        size = font.getsize(text)
+        draw.text((x - (size[0] / 2), y - (size[1] / 2)), text, font=font, fill=color)
+        brain = np.array(img_pil)
+        '''
+        # 텍스트 투명도 조절하는 부분(블렌딩 사용)
+        brain = cv.cvtColor(brain, cv.COLOR_BGR2BGRA)
+        #white = cv.cvtColor(white_bg, cv.COLOR_BGR2BGRA)
+        a = data.transparency / 100.0
+        b = 1.0 - a
+        brain = cv.addWeighted(brain, a, trans, b, 0)
+        '''
+    cv.imshow("whale brain", brain)
     del pre_data[:]  # 리스트 초기화
-    del pre_text[:]  # 리스트 초기화
-
-
-def del_typo(text):
-    flag = 0
-    for i in range(len(pre_text)):
-        if pre_text[i] == text:
-            data = pre_data[i]
-            data = brain_data(white_bg, '', data.hue, 0, data.point, 0)
-            pre_data[i] = whale_brain(data)
-            print_typo()
-            # print("삭제완료")
-            flag = 1
-    if flag == 0:
-        print("error: 입력하신 단어는 존재하지 않습니다")
 
 
 # 감정값 whale이미지에 맞게 비율 맞추는 함수
@@ -170,7 +172,7 @@ def deg2hue(degree):
         hue = int(degree) / 2
 
     # hue값 color(bgr)로 변환
-    Phsv = hsv(hue * 2, 1, 1)
+    Phsv = hsv(hue*2, 1, 1)
     Prgb = hsv2rgb(Phsv)
     r = int(Prgb.r * 255)
     g = int(Prgb.g * 255)
@@ -277,13 +279,13 @@ def color_line_chart(img, emotion):
     center_x = int(w / 2)
     center_y = int(h / 2)
 
-    result_x = int(center_x + emotion_x)  # 감정값의 원점을 이미지 중심으로 옮김
-    result_y = int(w - (center_y + emotion_y))  # y좌표의 0점이 화면상단에서 시작하므로 반전시킴
+    result_x = int(center_x + emotion_x)     # 감정값의 원점을 이미지 중심으로 옮김
+    result_y = int(w - (center_y + emotion_y))   # y좌표의 0점이 화면상단에서 시작하므로 반전시킴
 
-    color = deg2hue(xy2deg(emotion_x, emotion_y))  # 좌표에 따른 bgr값 받아옴
+    color = deg2hue(xy2deg(emotion_x, emotion_y))    # 좌표에 따른 bgr값 받아옴
 
-    cv.circle(img_circle, (center_x, center_y), 6, gray, -1)  # 그래프 중심
-    cv.arrowedLine(img_circle, (center_x, center_y), (result_x, result_y), color, 5, cv.CV_8UC3, 0, 0.1)  # 화살표그리기
+    cv.circle(img_circle, (center_x, center_y), 6, gray, -1)     # 그래프 중심
+    cv.arrowedLine(img_circle, (center_x, center_y), (result_x, result_y), color, 5, cv.CV_8UC3, 0, 0.1)    # 화살표그리기
 
     return img_circle
 
@@ -295,13 +297,13 @@ def stick_chart(img, mood, energy):
     result_x = int(mood * (180.0 / 100.0))
     result_y = int(energy * (180.0 / 100.0))
 
-    if mood >= 0:  # 감정의 x좌표(mood)가 양수이면 초록색으로 막대그래프 채움
+    if mood >= 0:   # 감정의 x좌표(mood)가 양수이면 초록색으로 막대그래프 채움
         img_stick = draw_rect(img_stick, 250, result_x, green)
-    else:  # 감정의 x좌표(mood)가 음수이면 보라색으로 막대그래프 채움
+    else:   # 감정의 x좌표(mood)가 음수이면 보라색으로 막대그래프 채움
         img_stick = draw_rect(img_stick, 250, result_x, purple)
     if energy >= 0:  # 감정의 y좌표(energy)가 양수이면 빨간색으로 막대그래프 채움
         img_stick = draw_rect(img_stick, 420, result_y, red)
-    else:  # 감정의 y좌표(energy)가 음수이면 파란색으로 막대그래프 채움
+    else:   # 감정의 y좌표(energy)가 음수이면 파란색으로 막대그래프 채움
         img_stick = draw_rect(img_stick, 420, result_y, blue)
 
     return img_stick
@@ -350,7 +352,7 @@ def trans_text_color(emotion, color, b_n_w):
 
     if b_n_w == 1:  # 흰배경일때
         Phsv = hsv(Phsv.h, Phsv.s * percent, Phsv.v)
-    else:  # 검은배경일때
+    else:   # 검은배경일때
         Phsv = hsv(Phsv.h, Phsv.s, Phsv.v * percent)
 
     Prgb = hsv2rgb(Phsv)
@@ -365,7 +367,7 @@ def trans_text_color(emotion, color, b_n_w):
 def emotion_word_point(img, p):
     img_grid = img.copy()
 
-    color = emotion_word_color(p)  # word의 위치좌표에 맞는 색상을 반환하는 함수
+    color = emotion_word_color(p)   # word의 위치좌표에 맞는 색상을 반환하는 함수
     cv.circle(img_grid, (p.x, p.y), 4, color, -1)
 
     return img_grid
@@ -385,104 +387,34 @@ def emotion_word_color(p):
     return deg2hue(xy2deg(word_x, word_y))
 
 
-def whale_brain(data):
-    brain = data.img.copy()
-
-    hue = float(450 - (data.hue * 2))
-    if hue > 360:
-        hue -= 360
-
-    color = deg2hue(hue)  # hue값 두배해서 넣어주는거임(==degree)
-    color = trans_text_color(data.transparency, color, 1)  # 투명도(0~100),색,(검은배경=0,흰배경=1)
-
-    p = data.point
-    if p[0] == 0:
-        x = 0
-    else:
-        x = p[0] * 3
-    if p[1] == 0:
-        y = 0
-    else:
-        y = p[1] * 3
-    x += 300
-    y = 600 - (y + 300)
-
-    text = data.word
-    font_scale = data.font_scale
-    fontpath = font_path
-    font = ImageFont.truetype(fontpath, font_scale)
-    img_pil = Image.fromarray(brain)
-    draw = ImageDraw.Draw(img_pil)
-    size = font.getsize(text)
-    # print(size)
-    draw.text((x - (size[0] / 2), y - (size[1] / 2)), text, font=font, fill=color)
-    brain = np.array(img_pil)
-    # cv.circle(brain,(x,y),5,black,-1)
-
-    '''
-    #puttext사용
-    #fontScale = int(data.font_scale / 10.0)
-    #thickness = fontScale+1
-    font = cv.FONT_HERSHEY_SIMPLEX
-    size = cv.getTextSize(text,font,fontScale,thickness)
-    #textRow, textCol = size
-    x = x - int(size[0][0]/2)
-    y = y + int(size[0][1]/2)
-    #word_center = Point(int(x - textRow/2),int(y + textCol/2))
-    cv.putText(brain,text,(x,y),font,fontScale,color,thickness)
-    '''
-
-    data = brain_data(brain, text, data.hue, data.transparency, data.point, font_scale)
-    # cv.imshow("whale brain",brain)
-
-    return data
-
-
-def hangulfont(data):
-    brain = white_bg.copy()
-    text_img = np.zeros((600, 600, 3), np.uint8)
-    fontpath = "malgun.ttf"
-    font = ImageFont.truetype(fontpath, 20)
-    img_pil = Image.fromarray(text_img)
-    draw = ImageDraw.Draw(img_pil)
-    draw.text((60, 60), "안녕하세요", font=font, fill=(255, 255, 255, 50))
-
-    cv.imshow("hangul", text_img)
-
-
 # 세가지의 창을 하나로 합치는 함수
-def combine_imshow(circleL, circleS, stick) -> object:
+def combine_imshow(circleL, circleS, stick) -> object :
     circleL = cv.resize(circleL, (600, 600))
     circleS = cv.resize(circleS, (380, 380))
     stick = cv.resize(stick, (380, 220))
-    # cv.imshow("circleL",circleL)
-    # cv.imshow("circleS",circleS)
-    # cv.imshow("stick",stick)
-    # opencv함수인 vconcat,hconcat함수가 제대로 작동하지 않아 numpy의 vstack,hstack함수로 변경
-    # vertical = cv.vconcat(circleS,stick)
-    # horizontal = cv.hconcat(circleL,vertical)
     vertical = np.vstack((circleS, stick))
     horizontal = np.hstack((circleL, vertical))
     cv.imshow("result", horizontal)
 
 
 if __name__ == "__main__":
-    gui_main((50, 50), (-30, -70), -60, 30)
-    brain_typo('잘되는구만', 20, 50, (0, 0), 100)
-    brain_typo('반가워', 70, 100, (70, -30), 40)
+    data1 = ['잘되는구만', 20, 50, (0, 0), 100]
+    data2 = ['반가워', 70, 100, (70, -30), 40]
+    data3 = ['봄이다', 0, 50, (70, 30), 70]
+    data4 = ['반가워', 120, 50, (-70, -30), 20]
+    data5 = ['봄봄', 150, 100, (-50, 20), 50]
+    data6 = ['잘되는구만', 70, 50, (0, 50), 50]
+    gui_graph((50, 50), (-30, -70), -60, 30)
+    brain_typo(data1)
+    brain_typo(data2)
     print_typo()
     cv.waitKey(0)
-    brain_typo('봄이다', 0, 80, (70, 30), 70)
-    brain_typo('안녕', 70, 100, (70, -30), 40)
-    brain_typo('잘되는구만', 30, 50, (0, 0), 60)
+    brain_typo(data3)
+    brain_typo(data4)
     print_typo()
     cv.waitKey(0)
-    gui_main((-50, 50), (45, -40), 10, -90)
-    brain_typo('반가워', 120, 50, (-70, -30), 20)
-    brain_typo('봄이다', 0, 80, (70, -30), 20)
-    print_typo()
-    cv.waitKey(0)
-    # brain_typo('잘되는구만', 70, 20, (0, 0), 10)
-    brain_typo('잘되는구만', 70, 100, (0, 50), 50)
+    gui_graph((-50, 50), (45, -40), 10, -90)
+    brain_typo(data5)
+    brain_typo(data6)
     print_typo()
     cv.waitKey(0)
