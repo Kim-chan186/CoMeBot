24944246837199aa0f8e2ec4cc7aa9c0f3995e20
Lin <50181023+Lin709@users.noreply.github.com>
2020-05-10 19:49:36 +0900

    소스 합침
    main.h에 따로 빼고,
    서버 통신부분과 vrep부분 합침

--- Simulation/Vrep/Header Files/Data_Format.h
+++ Simulation/Vrep/Header Files/Data_Format.h
@@ -2,10 +2,11 @@
 #include <string>
 #include <string.h>
 
-#define IP_ADDRESS "192.168.0.33"
+#define IP_ADDRESS "192.168.0.26"
+#define BUFSIZE 1024
 
 using namespace std;
-/*ID*/
+/* ID */
 #define CPP			0
 #define REI			1
 #define STT			2
@@ -18,26 +19,26 @@ using namespace std;
 /* SENSOR MODE*/	
 #define OFF			0
 #define ON			1
-/*OLED MODE*/
+/* OLED MODE */
 #define NORM		0
 #define HAPPY		1
 #define SAD			2
 #define ANGRY		3
 #define BORED		4
 #define FUN			5
-/*FIN MODE*/
+/* FIN MODE */
 #define SHAKE		0
 #define WING		1
 #define DOWN		2
 #define SLOW		3
 #define NOMOVE		4
-/*TAIL MODE*/
+/* TAIL MODE */
 #define FAST		0
 #define SLOW		1
 //#define DOWN		2
 #define UP			3
 //#define NOMOVE	4
-/*FACE DETECT & MOTIONFLAG*/
+/* FACE DETECT & MOTIONFLAG */
 #define NO			0
 #define YES			1
 
@@ -54,43 +55,40 @@ string Data_Packet[9][6] = {
 	{"m60","m61","m62","m63","m64"},		// TAIL (0~3)
 };
 string Mode[20] = {
-	"002",//NORM		SHAKE		DOWN
-	"020",//NORM		DOWN		FAST
-	"030",//NORM		SLOW		FAST
-	"011",//NORM		WING		SLOW
-	"100",//HAPPY		SHAKE		FAST
-	"101",//HAPPY		SHAKE		SLOW
-	"131",//HAPPY		SLOW		SLOW
-	"212",//SAD			WING		DOWN
-	"221",//SAD			DOWN		SLOW
-	"222",//SAD			DOWN		DOWN
-	"232",//SAD			SLOW		DOWN
-	"300",//ANGRY		SHAKE		FAST
-	"303",//ANGRY		SHAKE		UP
-	"313",//ANGRY		WING		UP
-	"323",//ANGRY		DOWN		UP
-	"421",//BORED		DOWN		SLOW
-	"422",//BORED		WING		DOWN
-	"431",//BORED		SLOW		SLOW
-	"510",//FUN			WING		FAST
-	"513",//FUN			WING		UP
+	"002", // NORM		SHAKE		DOWN
+	"020", // NORM		DOWN		FAST
+	"030", // NORM		SLOW		FAST
+	"011", // NORM		WING		SLOW
+	"100", // HAPPY		SHAKE		FAST
+	"101", // HAPPY		SHAKE		SLOW
+	"131", // HAPPY		SLOW		SLOW
+	"212", // SAD		WING		DOWN
+	"221", // SAD		DOWN		SLOW
+	"222", // SAD		DOWN		DOWN
+	"232", // SAD		SLOW		DOWN
+	"300", // ANGRY		SHAKE		FAST
+	"303", // ANGRY		SHAKE		UP
+	"313", // ANGRY		WING		UP
+	"323", // ANGRY		DOWN		UP
+	"421", // BORED		DOWN		SLOW
+	"422", // BORED		WING		DOWN
+	"431", // BORED		SLOW		SLOW
+	"510", // FUN		WING		FAST
+	"513", // FUN		WING		UP
 };
 
 /* Packet Global Variables */
-
-int Id = CPP;
-int Hungry_Para = 100;
-int Tired_Para = 100;
+int				  Id = CPP;
+int		 Hungry_Para = 100;
+int		  Tired_Para = 100;
 int Touch_Sensor = DEFAULT;
-int Force_Sensor = OFF;
-int	Lift_Sensor = OFF;
-int Oled_State = NORM;
-int Fin_State = NOMOVE;
-int Tail_State = NOMOVE;
-int Face_Detect = NO;
-int Reward = 0;
-int Mode_Select = 0;
-
-/* Global Variables */
+int		Force_Sensor = OFF;
+int		 Lift_Sensor = OFF;
+int		 Oled_State = NORM;
+int		Fin_State = NOMOVE;
+int	   Tail_State = NOMOVE;
+int		  Face_Detect = NO;
+int				Reward = 0;
+int		   Mode_Select = 0;
 
 
--- Simulation/Vrep/Source Files/main.cpp
+++ Simulation/Vrep/Source Files/main.cpp
@@ -1,75 +1,17 @@
-/*
-	TCP include Files
-*/
-#include "winsock2.h" // include before <windows.h>
-#include <conio.h>
-#define BUFSIZE 1024
-void ErrorHandling(char* message);
-/*
-	Vrep Include Files
-*/
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-#include <conio.h>
-#include <string>
-#include <string.h>
-#include <windows.h>
-
-
-/*
-	Thread Include Files
-*/
-#include <thread>
-#include <ctime>
-#include <mutex>
-#include <condition_variable>
-#include "typeinfo"
-using namespace std;
-
-
-extern "C" {
-#include "extApi.h"
-#include <v_repConst.h>
-
-	//#include "extApiPlatform.h" 
-}
-
-#define M_PI         (3.141592653589793238462643383279501884197169399375105829974944)
-#define TAB_COMMAND      9
-#define Q_COMMAND      113
-#define I_COMMAND      105
-#define W_COMMAND      119
-#define PORT         9200
-
-
-void init();
-void readdevice();
-void writedevice();
-void recv_socket(SOCKET sock);
-
-bool simulation_run = false;
-int _Init_walking_flag,
-_Walking_flag,
-_program_exit;
-int clientID;
-
-
-/* General Variables */
-simxInt   g_objHandle[6];
-simxInt come_objHandle[6];
-simxInt test_objHandle[2];
-/*/////////////////////////// */
-
-/* TCP Variables */
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include "main.h"
 #include "Data_Format.h"
-WSADATA wsaData;
-SOCKET hSocket;
-SOCKADDR_IN servAddr;
+using namespace std;
+using namespace cv;
 
+/* fps variables */
+chrono::system_clock::time_point	tpStart, tpEnd;
+double	 dTime = 0;
+int cont_while = 0;
 /* Mutex Variables */
 mutex mMutex;
 condition_variable mCondVar;
+
 struct SafeCondVar
 {
 	int m_iNC;
@@ -85,7 +27,7 @@ SafeCondVar::SafeCondVar()
 	: m_iNC(0), m_oMtx(), m_oCondVar()
 {
 }
-void SafeCondVar::notifyOne(){
+void SafeCondVar::notifyOne() {
 	lock_guard<mutex> oL(m_oMtx);
 	++m_iNC;
 	m_oCondVar.notify_one();
@@ -107,60 +49,60 @@ SafeCondVar Send_CondVar;
 SafeCondVar Recv_CondVar;
 /*/////////////////////////// */
 
-double   initialPos[6] = { 200 * M_PI / 180, 190 * M_PI / 180, 0, 0, M_PI, M_PI };
-double targetPos[3][6] = { { 90 * M_PI / 180, 90 * M_PI / 180, 90 * M_PI / 180, 90 * M_PI / 180, 90 * M_PI / 180, 90 * M_PI / 180 },
-					 { 90 * M_PI / 180,135 * M_PI / 180,225 * M_PI / 180,180 * M_PI / 180,180 * M_PI / 180,350 * M_PI / 180 },
-					 { 160 * M_PI / 180, 150 * M_PI / 180, 30 * M_PI / 180, 30 * M_PI / 180, 0, 0 } };
-double      initPos[2] = { 200 * M_PI / 180, 190 * M_PI / 180 };
-double      testPos[2] = { 160 * M_PI / 180, 150 * M_PI / 180 };
+
 
 /*///////////////////////////////////////////////////////////////////////////*/
-void init()
-{
-	_Init_walking_flag = false;
-	_Walking_flag = false;
-	_program_exit = false;
-}
-/*///////////////////////////////////////////////////////////////////////////*/
-void readdevice()
-{
-	simxFloat q_cur[2];
-	/*
-	simxFlo
- at *qc;   qc = q_cur;
-	for (int i = 0; i < 2; i++) {
-	   simxGetJointPosition(clientID, g_objHandle[i], qc, simx_opmode_streaming);
-	   }
-	*/
-
-	// comebot
-	for (int i = 0; i < 2; i++)
-		simxGetJointPosition(clientID, test_objHandle[0], &q_cur[i], simx_opmode_streaming);
-}
-/*///////////////////////////////////////////////////////////////////////////*/
-void writedevice()
+void writedevice(int n)
 {
 	/*
-	if (_Init_walking_flag == true) {
-	   for (int i = 0; i < 6; i++)
-		  simxSetJointTargetPosition(clientID, g_objHandle[i], initialPos[i], simx_opmode_streaming);
-	}
-	if (_Walking_flag == true) {
-	   for (int i = 1; i < 7; i++) {
-		  simxSetJointTargetPosition(clientID, g_objHandle[i - 1], targetPos[0][i - 1], simx_opmode_streaming);
-		  //simxSetJointTargetPosition(clientID, g_objHandle[j], g_q[j], simx_opmode_streaming);
-	   }
-	}
+		Motion Thread
+		int
 	*/
-
-	// comebot
-	if (_Init_walking_flag == true) {
-		for (int i = 0; i < 4; i++)
-			simxSetJointTargetPosition(clientID, come_objHandle[i], initialPos[i], simx_opmode_streaming);
+	switch (n) {
+	case 1:		front();	break;
+	case 2:		left();		break;
+	case 3:		right();	break;
+	default:	back();		break;
 	}
-	if (_Walking_flag == true) {
-		for (int i = 0; i < 4; i++)
-			simxSetJointTargetPosition(clientID, come_objHandle[i], targetPos[2][i], simx_opmode_streaming);
+}
+/*///////////////////////////////////////////////////////////////////////////*/
+simxFloat force_cur = -0.182;
+void forcesensor()
+{
+	simxReadForceSensor(clientID, force_Handle, NULL, &force_cur, NULL, simx_opmode_streaming);
+}
+/*///////////////////////////////////////////////////////////////////////////*/
+simxFloat getposition[3] = { 0, };
+void getpos()
+{
+	simxGetObjectPosition(clientID, body_Handle, -1, getposition, simx_opmode_blocking);
+}
+/*///////////////////////////////////////////////////////////////////////////*/
+simxUChar*	   comeimage = 0;
+simxInt		   resolution[2];
+char _fps[5];	  string fps;
+void getimage()
+{
+	cv::namedWindow("vrep", CV_WINDOW_AUTOSIZE);
+	while (1) {//for (int time = 0; time < 1000; time++) {
+		int retval = simxGetVisionSensorImage(clientID, image_Handle, resolution, &comeimage, 0, simx_opmode_streaming);
+		if (retval != simx_return_ok) {
+			continue;
+		}
+		_itoa_s(dTime + 0.1, _fps, 5, 10);	fps = _fps;
+		Mat img(resolution[0], resolution[1], CV_8UC3, comeimage);
+		flip(img, img, 0);
+		cvtColor(img, img, cv::COLOR_RGB2BGR);
+		putText(img, "FPS:" + fps, Point(430, 30), FONT_HERSHEY_PLAIN, 1.2, Scalar(255, 230, 100));
+		cv::imshow("vrep", img);
+
+		if (++cont_while == 1000) {
+			cont_while = 0;
+			tpEnd = chrono::system_clock::now();
+			dTime = 100000.0 / chrono::duration_cast<chrono::milliseconds>(tpEnd - tpStart).count();
+			tpStart = tpEnd;
+		}
+		cv::waitKey(27);
 	}
 }
 /*///////////////////////////////////////////////////////////////////////////*/
@@ -187,18 +129,12 @@ void recv_socket(SOCKET sock) {
 		if (bytesReceived > 0)
 		{
 			ibuff = atoi(buff); // buff : char[] -> int
-			//mode_buff = Mode[ibuff].c_str();
 			ibuff = atoi(Mode[ibuff].c_str());
-			//mMutex.lock();
+
 			Oled_State = ibuff / 100;
 			Fin_State = (ibuff % 100) / 10;
 			Tail_State = ibuff % 10;
-			//mMutex.unlock();
 			printf("\nrecv: %s\n", &buff);
-			//printf("%d %d %d\n", Oled_State, Fin_State, Tail_State);
-			//cout << typeid(ibuff).name() << endl;
-			//cout << typeid(Oled_State).name() << endl;
-			//cout << "SERVER> " << string(buff, 0, bytesReceived) << endl;
 		}
 		Recv_CondVar.notifyOne();
 	}
@@ -208,155 +144,72 @@ void tran_socket(SOCKET sock) {
 	char cMsg[] = "";
 	string packet;
 	int i = 0;
-	
 
 	/* ID Change */
-	//lock.unlock();
 	scanf_s("%d", &Id); //0 cpp 1 rei 2 stt
-	//Id = 0;
- 	strcpy(cMsg, Data_Packet[0][Id].c_str()); //id : string -> char[]
+	strcpy(cMsg, Data_Packet[0][Id].c_str()); //id : string -> char[]
 	send(sock, cMsg, strlen(cMsg), 0); // id setting in Server
-	
+
 	while (1) {
-		//unique_lock<mutex> lock(mMutex);
-		//mCondVar.wait(lock, []()->bool {return MotionEndFlag; });
 		Send_CondVar.wait();
-		
+
 		packet =
-			Data_Packet[0][Id]				+ ","
-			+ to_string(Hungry_Para)		+ ","
-			+ to_string(Tired_Para)			+ ","
-			+ Data_Packet[3][Touch_Sensor]	+ ","
-			+ Data_Packet[4][Force_Sensor]	+ ","
-			+ Data_Packet[5][Lift_Sensor]	+ ","
-			+ Data_Packet[6][Oled_State]	+ ","
-			+ Data_Packet[7][Fin_State]		+ ","
-			+ Data_Packet[8][Tail_State]	+ ","
-			+ to_string(Oled_State)			
-			+ to_string(Fin_State)			
-			+ to_string(Tail_State)			+ ","
-			+ to_string(Face_Detect)		+ ","
-			+ to_string(Reward);			
+			Data_Packet[0][Id] + ","
+			+ to_string(Hungry_Para) + ","
+			+ to_string(Tired_Para) + ","
+			+ Data_Packet[3][Touch_Sensor] + ","
+			+ Data_Packet[4][Force_Sensor] + ","
+			+ Data_Packet[5][Lift_Sensor] + ","
+			+ Data_Packet[6][Oled_State] + ","
+			+ Data_Packet[7][Fin_State] + ","
+			+ Data_Packet[8][Tail_State] + ","
+			+ to_string(Oled_State)
+			+ to_string(Fin_State)
+			+ to_string(Tail_State) + ","
+			+ to_string(Face_Detect) + ","
+			+ to_string(Reward);
 		strcpy(cMsg, packet.c_str()); //packet : string -> char[]
 		/*Packet 전송*/
 		send(sock, cMsg, strlen(cMsg), 0);
 		printf("\nSend_Packet: %s\n", cMsg);
-	
+
 		Sleep(10);
 	}
 }
 void motion_thread() {
 	int distribute = 0;
-	while(1){
+	while (1) {
 		Recv_CondVar.wait();
-		//unique_lock<mutex> lock(mMutex);
 		distribute = stoi(Mode[Mode_Select]);
 		//Oled_State = distribute / 100;
 		//Fin_State = (distribute % 100) / 10;
-		//Tail_State = distribute % 10;
+
+
+			//Tail_State = distribute % 10;
 		printf("Motion : %d %d %d \n", Oled_State, Fin_State, Tail_State);
 		/*Motion Function*/
 		Sleep(2000);
 		/*Motion End*/
-		
+
 		Send_CondVar.notifyOne();
-		//mCondVar.notify_all();		
 	}
 }
 
-int main(int argc, char* argv[])
+void vrepandact()
 {
-	/* Main Variables */
-	//char cMsg[] = "";
-	//string packet;
-	//int i = 0;
-	Hungry_Para = 50;
-
-	/* TCP Transmission_Init */
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) //소켓 라이브러리를 초기화하고 있다
-		ErrorHandling("WSAStartup() error!");
-	hSocket = socket(PF_INET, SOCK_STREAM, 0); //소켓을 생성
-	if (hSocket == INVALID_SOCKET)
-		ErrorHandling("socket() error");
-	memset(&servAddr, 0, sizeof(servAddr));
-	servAddr.sin_family = AF_INET;
-	servAddr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
-	servAddr.sin_port = htons(8585);
-	if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR) //생성된 소켓을 바탕으로 서버에 연결요청을 하고 있다
-		ErrorHandling("connect() error!");
-
-	/* Thread_Init */
-	thread recv;
-	thread tran;
-	thread para;	
-	thread vrep;
-	
-	para = thread(Parameter_Thread);
-	vrep = thread(motion_thread);
-	tran = thread(tran_socket, hSocket);
-	recv = thread(recv_socket, hSocket);
-	
-	///* ID Change */
-	//scanf_s("%d", &Id); //0 cpp 1 rei 2 stt
-
-	//strcpy(cMsg, Data_Packet[0][Id].c_str()); //id : string -> char[]
-	//send(hSocket, cMsg, strlen(cMsg), 0); // id setting in Server
-	//while (1) {		
-	//	packet = 
-	//		 Data_Packet[0][Id]				+ "," 
-	//		+to_string(Hungry_Para)			+ "," 
-	//		+to_string(Tired_Para)			+ ","
-	//		+Data_Packet[3][Touch_Sensor]	+ ","
-	//		+Data_Packet[4][Force_Sensor]	+ ","
-	//		+Data_Packet[5][Lift_Sensor]	+ ","
-	//		+Data_Packet[6][Oled_State]		+ ","
-	//		+Data_Packet[7][Fin_State]		+ ","
-	//		+Data_Packet[8][Tail_State]		+ ","
-	//		+to_string(Oled_State)
-	//		+to_string(Fin_State)
-	//		+to_string(Tail_State)			+ ","
-	//		+to_string(Reward);
-
-	//	strcpy(cMsg, packet.c_str()); //packet : string -> char[]
-	//	//Packet 전송
-	//	send(hSocket, cMsg, strlen(cMsg), 0);
-
-	//	printf("\nSend_Packet: %s\n", cMsg);
-	//	Sleep(1000);
-	//}
-	recv.join();
-	para.join();
-	tran.join();
-	vrep.join();
-	closesocket(hSocket); //소켓 라이브러리 해제
-	WSACleanup();
-
-
 	//////////////////////////////////////////////////////////////////////////
-	printf("Start Program\n");
 	clientID = simxStart((simxChar*)"127.0.0.1", 9200, true, true, 5000, 5);
 
-
 	/*                get handle                 */
-	/*
-	simxGetObjectHandle(clientID, "Jaco_joint1", &g_objHandle[0], simx_opmode_oneshot_wait);
-	simxGetObjectHandle(clientID, "Jaco_joint2", &g_objHandle[1], simx_opmode_oneshot_wait);
-	simxGetObjectHandle(clientID, "Jaco_joint3", &g_objHandle[2], simx_opmode_oneshot_wait);
-	simxGetObjectHandle(clientID, "Jaco_joint4", &g_objHandle[3], simx_opmode_oneshot_wait);
-	simxGetObjectHandle(clientID, "Jaco_joint5", &g_objHandle[4], simx_opmode_oneshot_wait);
-	simxGetObjectHandle(clientID, "Jaco_joint6", &g_objHandle[5], simx_opmode_oneshot_wait);
-	*/
-
-	//// comebot
+	simxGetObjectHandle(clientID, "body", &body_Handle, simx_opmode_oneshot_wait);
 	simxGetObjectHandle(clientID, "body_tail1", &come_objHandle[0], simx_opmode_oneshot_wait);
 	simxGetObjectHandle(clientID, "tail1_tail2", &come_objHandle[1], simx_opmode_oneshot_wait);
-	//
 	simxGetObjectHandle(clientID, "wing_left_Motor", &come_objHandle[2], simx_opmode_oneshot_wait);
 	simxGetObjectHandle(clientID, "wing_right_Motor", &come_objHandle[3], simx_opmode_oneshot_wait);
-	////
-	//simxGetObjectHandle(clientID, "left_wheel_Motor", &come_objHandle[4], simx_opmode_oneshot_wait);
-	//simxGetObjectHandle(clientID, "right_wheel_Motor", &come_objHandle[5], simx_opmode_oneshot_wait);
-	////
+	simxGetObjectHandle(clientID, "left_wheel_Motor", &come_objHandle[4], simx_opmode_oneshot_wait);
+	simxGetObjectHandle(clientID, "right_wheel_Motor", &come_objHandle[5], simx_opmode_oneshot_wait);
+	simxGetObjectHandle(clientID, "camera", &image_Handle, simx_opmode_oneshot_wait);
+	simxGetObjectHandle(clientID, "shock", &force_Handle, simx_opmode_oneshot_wait);
 
 
 	int n = simxSynchronous(clientID, true);
@@ -373,83 +226,141 @@ int main(int argc, char* argv[])
 	if (clientID != -1) {
 		printf("Connection Established\n");
 		init();
-
 		while (simxGetConnectionId(clientID) != -1)
 		{
-			readdevice();
 			if (kbhit())
 			{
 				int key = _getch();
 				printf("%d\t", key);
 
 				switch (key) {
-				case I_COMMAND:
-					cout << " Initial Pos\n" << endl;
-					_Init_walking_flag = true;
-					_Walking_flag = false;
+				case S_COMMAND:
+					std::cout << "Back\n";
+					_Back_flag = true;		_Front_flag = false;	_Left_flag = false;		_Right_flag = false;
 					break;
-
 				case W_COMMAND:
-					cout << " Put\n" << endl;
-					_Walking_flag = true;
-					_Init_walking_flag = false;
-					//_WalkingCtrl._initialize();
-					//_WalkingCtrl.StartWalking();
-					//_WalkingCtrl.setApproachdata(0.0,0.0,50*DEGREE);
+					std::cout << "Front\n";
+					_Front_flag = true;		_Back_flag = false;		_Left_flag = false;		_Right_flag = false;
+					break;
+				case A_COMMAND:
+					std::cout << "LEF\n";
+					_Left_flag = true;		_Front_flag = false;	_Back_flag = false;		_Right_flag = false;
+					break;
+				case D_COMMAND:
+					std::cout << "RIG\n";
+					_Right_flag = true;		_Left_flag = false;		_Front_flag = false;	_Back_flag = false;
+					break;
+				case R_COMMAND:
+					std::cout << "Read\n";
+					if (_Reading_flag)		_Reading_flag = false;	else					_Reading_flag = true;
 					break;
 
 				case TAB_COMMAND:
 					if (simulation_run) {
 						simulation_run = false;
-						cout << " Stop\n" << endl;
+						std::cout << "Stop\n";
 					}
 					else {
 						simulation_run = true;
-						cout << "  Go\n" << endl;
+						std::cout << "Go\n";
 					}
 					break;
-
-				case Q_COMMAND:
-					cout << " Program End\n" << endl;
-					simulation_run = false;
-					_program_exit = true;
-					break;
-
 				default:
 					break;
 				}
 			}
 
+
+
+			// 만약 충격이나 들어올림 플래그가 서면 따로 저장해두고 그 패킷 보내기
+
+
+
+
+
+
+
 			if (simulation_run == true)
 			{
-				if (_Init_walking_flag == true) {
+				if (_Back_flag == true) {
 					/*      initial Pos      */
-					writedevice();
+					num = 0;
+					writedevice(num);
 					simxSynchronousTrigger(clientID);
 				}
-				else if (_Walking_flag == true) {
-					/*      target Pos      */
-					writedevice();
+				else if (_Front_flag == true) {
+					num = 1;
+					writedevice(num);
 					simxSynchronousTrigger(clientID);
+				}
+				else if (_Left_flag == true) {
+					num = 2;
+					writedevice(num);
+					simxSynchronousTrigger(clientID);
+				}
+				else if (_Right_flag == true) {
+					num = 3;
+					writedevice(num);
+					simxSynchronousTrigger(clientID);
+				}
+				if (_Reading_flag == true) {
+					forcesensor();			getpos();
+					simxSynchronousTrigger(clientID);
+					if ((force_cur < -0.1805) & (force_cur > -0.1835))	force_flag = 0;
+					else												force_flag = 1;
+					printf("\t%d", force_flag);//printf("%lf\n", force_cur);
+					printf("\t%f\t%f\t%f\n", getposition[0], getposition[1], getposition[2]);
 				}
 			}
 			Sleep(0);
 		}
+		simxStopSimulation(clientID, simx_opmode_oneshot);
 		simxFinish(clientID);
 	}
-	return 0;	
 }
 
+int main(int argc, char* argv[])
+{
+	Hungry_Para = 50;
 
-/*
-	TCP Transmission User Func List
-*/
-//////////////////////////////////////////////////////////////////////////
-void ErrorHandling(char* message) {
+	/* TCP Transmission_Init */
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) //소켓 라이브러리를 초기화하고 있다
+		ErrorHandling("WSAStartup() error!");
+	hSocket = socket(PF_INET, SOCK_STREAM, 0); //소켓을 생성
+	if (hSocket == INVALID_SOCKET)
+		ErrorHandling("socket() error");
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
+	servAddr.sin_port = htons(8585);
+	if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR) //생성된 소켓을 바탕으로 서버에 연결요청을 하고 있다
+		ErrorHandling("connect() error!");
+
+
+	printf("Start Program\n");
+	/* Thread_Init */
+	thread recv;
+	thread tran;
+	thread para;
+	thread vrep;
+	thread recvimg;
+	thread vrepcon;
+
+	para = thread(Parameter_Thread);
+	vrep = thread(motion_thread);
+	tran = thread(tran_socket, hSocket);
+	recv = thread(recv_socket, hSocket);
+	recvimg = thread(getimage);
+	vrepcon = thread(vrepandact);
+
+	recv.join();
+	para.join();
+	tran.join();
+	vrep.join();
+	recvimg.join();
+	vrepcon.join();
+	closesocket(hSocket); //소켓 라이브러리 해제
 	WSACleanup();
-	fputs(message, stderr);
-	fputc('\n', stderr);
-	_getch();
-	exit(1);
+
+	return 0;
 }
-//////////////////////////////////////////////////////////////////////////
